cecaf11ef5b68c8671cc73c03097a03b58c761e3
Léa <lesanche@student42.fr>
2018-11-14 23:32:09 +0100

    your eurodep

---
+++ quiz.js
@@ -0,0 +1,28 @@
+var quiz = [
+    {
+      question: "Quel est votre genre ?",
+      type: "choice",
+      choices: ["femme", "homme"],
+    },
+    {
+      question: "Quel est votre prénom ?",
+      type: "text"
+    },
+    {
+      question: "Quelle est votre circonscription ?",
+      type: "choice",
+      choices: ["Nord-Ouest", "Ouest", "Est", "Sud-Ouest", "Sud-Est", "Massif-central Centre", "Ile-de-France et Français HF", "Outre-Mer"],
+    },
+    {
+      question: "Quelle est votre année de naissance ?",
+      type: "number"
+    },
+    {
+      question: "Quelles sont vos affinités politiques ?",
+      type: "choice",
+      choices: ["Front de gauche / Parti communiste", "Europe Écologie / Génération.s, le mouvement",
+      "Parti socialiste / Parti radical", "UDI / Modem / En Marche ", "Les Républicains et apparentés", 
+      "Rassemblement national / Les Patriotes / Debout la France / Apparentés", 
+      "Indépendant / Autre"],
+    }
+  ];
